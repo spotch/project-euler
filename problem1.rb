8afa567ff98ca6c5219f074ff44ac74c01fc46b9
Adam Schneider (linux) <ajs.schneider@gmail.com>
2012-12-22 12:01:42 -0600

    - pushing problem1 into an object

--- problem1.rb
+++ problem1.rb
@@ -1,10 +1,25 @@
 
-sum = 0
-(0..999).step(3) {|x| sum += x }
-(0..999).step(5) {|x|
-	if (x % 3 != 0)
-		sum += x
-	end
-}
+class Problem1
 
-puts sum
+	def init
+
+	end
+
+	def DetermineSumOfMultiplesOfThreeAndFive(lessThanThisNumber)
+		sum = 0
+		(0..999).step(3) {|x| sum += x }
+		(0..999).step(5) {|x|
+		   if (x % 3 != 0)
+		      sum += x
+		   end
+		}
+		return sum
+	end
+
+end
+
+if __FILE__ == $0 then
+
+	puts Problem1.new.DetermineSumOfMultiplesOfThreeAndFive(1000)
+
+end
