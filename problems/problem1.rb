c7a379cddc733498b222092dabb5b122cb02be7c
Adam Schneider (linux) <ajs.schneider@gmail.com>
2012-12-25 20:33:45 -0600

    - adding description to problem 1  - moving problems into problem directory

--- problems/problem1.rb
+++ problems/problem1.rb
@@ -18,6 +18,9 @@ class Problem1
 
 end
 
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
 if __FILE__ == $0 then
 
 	puts Problem1.new.DetermineSumOfMultiplesInRange(3, 5, 0..999)
