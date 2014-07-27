ed0f699cf02abf254fb9f8925eee706309c4c6e5
Adam Schneider <spotchious@gmail.com>
2014-07-27 14:50:40 -0500

    - robustisizing problem1

--- haskell/problem1.hs
+++ haskell/problem1.hs
@@ -1,1 +1,5 @@
-main = print (sum [ x | x <- [1..999], mod x 3 == 0 || mod x 5 == 0 ])
+inputRange = [1..999]
+divisibleByThreeOrFive x = mod x 3 == 0 || mod x 5 == 0
+sumNumbers range criteria = sum [x | x <- range, criteria x]
+
+main = print (sumNumbers inputRange divisibleByThreeOrFive)
